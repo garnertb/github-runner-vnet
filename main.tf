7b1cd8e30883aa68e64c06a92e35dc377fc084e8
Tyler Garner <garnertb@gmail.com>
2023-11-07 15:20:48 -0500

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,137 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3.0.0"
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "actions_NSG" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "DenyInternetOutBoundOverwrite"
+    priority                   = 400
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+
+  security_rule {
+    name                         = "AllowVnetOutBoundOverwrite"
+    source_port_range            = "*"
+    destination_port_range       = "443"
+    source_address_prefix        = "*"
+    destination_address_prefix   = "VirtualNetwork"
+    access                       = "Allow"
+    priority                     = 200
+    direction                    = "Outbound"
+    destination_address_prefixes = []
+    protocol                     = "Tcp"
+  }
+
+  security_rule {
+    name                         = "AllowAzureCloudOutBound"
+    source_port_range            = "*"
+    destination_port_range       = "443"
+    source_address_prefix        = "*"
+    destination_address_prefix   = "AzureCloud"
+    access                       = "Allow"
+    priority                     = 210
+    direction                    = "Outbound"
+    destination_address_prefixes = []
+    protocol                     = "Tcp"
+  }
+
+  security_rule {
+    name                   = "AllowInternetOutBoundGitHub"
+    protocol               = "Tcp"
+    source_port_range      = "*"
+    destination_port_range = "443"
+    source_address_prefix  = "*"
+    access                 = "Allow"
+    priority               = 220
+    direction              = "Outbound"
+    destination_address_prefixes = [
+      "140.82.112.0/20",
+      "142.250.0.0/15",
+      "143.55.64.0/20",
+      "192.30.252.0/22",
+      "185.199.108.0/22"
+    ]
+  }
+
+  security_rule {
+    name                   = "AllowInternetOutBoundMicrosoft"
+    protocol               = "Tcp"
+    source_port_range      = "*"
+    destination_port_range = "443"
+    source_address_prefix  = "*"
+    access                 = "Allow"
+    priority               = 230
+    direction              = "Outbound"
+    destination_address_prefixes = [
+      "13.64.0.0/11",
+      "13.96.0.0/13",
+      "13.104.0.0/14",
+      "20.33.0.0/16",
+      "20.34.0.0/15",
+      "20.36.0.0/14",
+      "20.40.0.0/13",
+      "20.48.0.0/12",
+      "20.64.0.0/10",
+      "20.128.0.0/16",
+      "52.224.0.0/11",
+      "204.79.197.200"
+    ]
+  }
+
+  security_rule {
+    name                         = "AllowInternetOutBoundCannonical"
+    protocol                     = "Tcp"
+    source_port_range            = "*"
+    destination_port_range       = "443"
+    source_address_prefix        = "*"
+    access                       = "Allow"
+    priority                     = 240
+    direction                    = "Outbound"
+    destination_address_prefix   = "185.125.188.0/22"
+    destination_address_prefixes = []
+  }
+}
+
+resource "azurerm_virtual_network" "ghvnet" {
+  name                = "actions-vnet"
+  location            = var.location
+  resource_group_name = azurerm_network_security_group.actions_NSG.resource_group_name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "actions-subnet"
+  resource_group_name  = azurerm_network_security_group.actions_NSG.resource_group_name
+  virtual_network_name = azurerm_virtual_network.ghvnet.name
+
+  address_prefixes = var.address_prefixes
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name = "GitHub.Network/networkSettings"
+    }
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.actions_NSG.id
+}
---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "location" {
+  description = "NSG for outbound rules"
+  type        = string
+  default     = "eastus"
+}
+
+variable "nsg_name" {
+  description = "NSG Name"
+  type        = string
+  default     = "actions_nsg"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group in which to create the NSG"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "address_prefixes" {
+  description = "Address prefixes for the subnet"
+  type        = list(string)
+  default     = ["10.0.0.0/24"]
+}
