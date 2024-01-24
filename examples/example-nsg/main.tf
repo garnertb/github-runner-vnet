9de7407d532b2ced4893327b6ec9220b17e37bd3
Tyler Garner <garnertb@github.com>
2024-01-24 11:26:20 -0500

    Update examples/example-nsg/main.tf

--- examples/example-nsg/main.tf
+++ examples/example-nsg/main.tf
@@ -1,6 +1,6 @@
 # Create a VNET using an Azure Network Security Group to control the VNET network access
 module "vnet" {
-    source = "github.com/pwideman/github-runner-vnet//modules/nsg"
+    source = "github.com/garnertb/github-runner-vnet//modules/nsg"
 
     base_name = var.base_name
     github_org_id = var.github_org_id
