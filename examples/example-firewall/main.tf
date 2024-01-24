b7f43b817c2c1e1620ced8bc2d10ffcc5a926a85
Tyler Garner <garnertb@github.com>
2024-01-24 11:26:07 -0500

    Update examples/example-firewall/main.tf

--- examples/example-firewall/main.tf
+++ examples/example-firewall/main.tf
@@ -1,6 +1,6 @@
 # Create a VNET using an Azure Firewall to control the VNET network access
 module "vnet" {
-    source = "github.com/pwideman/github-runner-vnet//modules/firewall"
+    source = "github.com/garnertb/github-runner-vnet//modules/firewall"
 
     base_name = var.base_name
     github_org_id = var.github_org_id
